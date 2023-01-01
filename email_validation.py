89f7924dd21d30256ee3fcffe40b5ada00a52106
MohitKumar020291 <120097428+MohitKumar020291@users.noreply.github.com>
2023-01-01 19:20:51 +0530

    Update email_validation.py

--- email_validation.py
+++ email_validation.py
@@ -1,9 +1,9 @@
 email = input("Enter your Email: ") #a@g.com
 k,j,d  = 0,0,0
 if len(email) >= 7: # checkihng whether min length of stadard email satisfy or not 
-    if email[0].isalpha():
-        if ("@" in email) and (email.count("@") == 1):
-            if (email[-4] == ".") ^ (email[-3] == "."): #iska matlab hai .com ya .in ^ means dono me se ke true
+    if email[0].isalpha(): #checking the first letter of the email is alphabet or not
+        if ("@" in email) and (email.count("@") == 1): # checking @ present in email or not and not more than once
+            if (email[-4] == ".") ^ (email[-3] == "."): #this is checking letter by index method -4(.com) and -3(.in)
                 for i in email: #using for loop to check each letter of string(email)
                     if i == i.isspace():
                         k = 1
